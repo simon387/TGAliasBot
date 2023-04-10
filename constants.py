4ce3e920a39f342f44b713d27374e46fea955364
Simone Celia <simone.celia@simonecelia.it>
2023-04-10 22:21:47 +0200

    init

---
+++ constants.py
@@ -0,0 +1,37 @@
+import configparser
+import logging
+
+config = configparser.RawConfigParser()
+config.read("config.properties")
+# application's secrets
+SECRETS = "secrets"
+TOKEN = config.get(SECRETS, "telegram.token")
+TELEGRAM_GROUP_ID = config.get(SECRETS, "telegram.group.id")
+TELEGRAM_DEVELOPER_CHAT_ID = config.get(SECRETS, "telegram.developer.chat.id")
+# application's settings
+APPLICATION = "application"
+SEND_START_AND_STOP_MESSAGE = config.get(APPLICATION, "send.start.and.stop.message")
+HTTP_VERSION = config.get(APPLICATION, "http.version")
+AIO_RATE_LIMITER_MAX_RETRIES = 10
+case = config.get(APPLICATION, "log.level")
+if case == "info":
+	LOG_LEVEL = logging.INFO
+elif case == "debug":
+	LOG_LEVEL = logging.DEBUG
+elif case == "error":
+	LOG_LEVEL = logging.ERROR
+else:
+	LOG_LEVEL = logging.DEBUG
+# messages
+STARTUP_MESSAGE = "TGAliasBot started! "
+STOP_MESSAGE = "TGAliasBot stopped!"
+VERSION_MESSAGE = " - more info on https://github.com/simon387/TGAliasBot/blob/master/changelog.txt"
+ERROR_NO_GRANT_SHUTDOWN = "You can't shutdown the bot!"
+# urls
+
+# var
+SPACE = " "
+EMPTY = ""
+MP3 = "MP3"
+MP4 = "MP4"
+TRUE = "true"
